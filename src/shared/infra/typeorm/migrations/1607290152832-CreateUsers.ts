3f3edc4d52f068afe00c6ba7ecfff2369cf5a779
Guilherme Henrique <guiihdourado@gmail.com>
2021-03-28 19:10:55 -0300

    Change name table in migration users

--- src/shared/infra/typeorm/migrations/1607290152832-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1607290152832-CreateUsers.ts
@@ -42,6 +42,6 @@ export default class CreateUsers1607290152832 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
